73697e4032a55f3437d4ba02b4734dfa629a061e
Max Schwarz <max.schwarz@online.de>
2019-07-27 19:13:51 +0200

    topic_manager: warn about & ignore duplicate topics

--- src/topic_manager.cpp
+++ src/topic_manager.cpp
@@ -4,6 +4,9 @@
 #include "topic_manager.h"
 
 #include <ros/node_handle.h>
+#include <rosfmt/rosfmt.h>
+
+#include <ros/names.h>
 
 namespace rosbag_fancy
 {
@@ -22,7 +25,22 @@ TopicManager::TopicManager()
 
 void TopicManager::addTopic(const std::string& topic, float rateLimit)
 {
-	m_topics.emplace_back(topic, rateLimit);
+	std::string resolvedName = ros::names::resolve(topic);
+
+	auto it = std::find_if(m_topics.begin(), m_topics.end(), [&](Topic& t){
+		return t.name == resolvedName;
+	});
+
+	if(it != m_topics.end())
+	{
+		ROSFMT_WARN(
+			"You tried to record topic '{}' twice. I'll ignore that (and use the first rate limit given, if applicable)",
+			resolvedName
+		);
+		return;
+	}
+
+	m_topics.emplace_back(resolvedName, rateLimit);
 }
 
 void TopicManager::updateStatistics()
